5ef2188b2dc4cbca203b243af16858b717b5af8a
kryshen <kryshen@mail.ru>
2022-05-24 14:41:17 +0300

    Lesson 5 - 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-//import axios from 'axios'
-import {HashRouter, BrowserRouter, Route, Link} from 'react-router-dom'
+import axios from 'axios'
+import {HashRouter, BrowserRouter, Route, NavLink} from 'react-router-dom'
 
 import UserList from './components/Users.js'
 import ProjectList from './components/Projects.js'
@@ -12,71 +12,101 @@ class App extends React.Component {
 
     constructor(props) {
         super(props)
-
-        const user1 = {first_name: 'Иван', last_name: 'Иванов', role: 'manager'}
-        const user2 = {first_name: 'Пётр', last_name: 'Петров', role: 'developer'}
-        const users = [user1, user2]
-
-        const project1 = {name: 'Project 1', owner: user1}
-        const project2 = {name: 'Project 2', owner: user1}
-        const project3 = {name: 'Project 3', owner: user2}
-        const project4 = {name: 'Project 4', owner: user2}
-        const projects = [project1, project2, project3, project4]
-
-        const task1 = {project: project1, description: 'task 1', assigned_to: '123'}
-        const task2 = {project: project1, description: 'task 2', assigned_to: '123'}
-        const task3 = {project: project2, description: 'task 3', assigned_to: '123'}
-        const task4 = {project: project2, description: 'task 4', assigned_to: '123'}
-        const tasks = [task1, task2, task3, task4]
-
         this.state = {
-            'users': users,
-            'projects': projects,
-            'tasks': tasks
+            'users': [],
+            'projects': [],
+            'tasks': []
         }
+    }
+    componentDidMount() {
+
+        const requestUser = 'http://127.0.0.1:8000/api/users'
+        const requestProject = 'http://127.0.0.1:8000/api/projects'
+        const requestTask = 'http://127.0.0.1:8000/api/tasks'
+
+        axios.all([requestUser, requestProject, requestTask])
+            .then(axios.spread((responses) => {
+                const responseOne = responses[0].data.responses
+                const responseTwo = responses[1].data.responses
+                const responseThree = responses[2].data.responses
+
+                console.log(responseOne, responseTwo, responseThree)
+                })
+            )
+            .catch(errors => {console.error(errors)
+            })
     }
 
     render() {
         return (
-            <div className="App">
-
                 <BrowserRouter>
-                    <nav>
+                    <div className="App">
                         <ul>
                             <li>
-                            <Link to='/'>Users</Link>
+                            <NavLink to='/'>Users</NavLink>
                             </li>
                             <li>
-                            <Link to='/projects'>Projects</Link>
+                            <NavLink to='/projects'>Projects</NavLink>
                             </li>
                             <li>
-                            <Link to='/tasks'>Tasks</Link>
+                            <NavLink to='/tasks'>Tasks</NavLink>
                             </li>
                         </ul>
-                    </nav>
                     <Route exact path='/' component={() => <UserList users={this.state.users} />} />
                     <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects} />} />
                     <Route exact path='/tasks' component={() => <TaskList tasks={this.state.tasks} />} />
+                        </div>
+
                 </BrowserRouter>
-            </div>
         )
     }
 }
 
+
 export default App;
+
+//    constructor(props) {
+//        super(props)
+//        const user1 = {first_name: 'Иван', last_name: 'Иванов', role: 'manager'}
+//        const user2 = {first_name: 'Пётр', last_name: 'Петров', role: 'developer'}
+//        const users = [user1, user2]
+//
+//        const project1 = {name: 'Project 1', owner: user1}
+//        const project2 = {name: 'Project 2', owner: user1}
+//        const project3 = {name: 'Project 3', owner: user2}
+//        const project4 = {name: 'Project 4', owner: user2}
+//        const projects = [project1, project2, project3, project4]
+//
+//        const task1 = {project: project1, description: 'task 1', assigned_to: '123'}
+//        const task2 = {project: project1, description: 'task 2', assigned_to: '123'}
+//        const task3 = {project: project2, description: 'task 3', assigned_to: '123'}
+//        const task4 = {project: project2, description: 'task 4', assigned_to: '123'}
+//        const tasks = [task1, task2, task3, task4]
+//
+//        this.state = {
+//            'users': users,
+//            'projects': projects,
+//            'tasks': tasks
+//        }
+//    }
+
 //    componentDidMount() {
-//        axios.get('http://127.0.0.1:8000/api/users')
-//            .then(response => {
-//                const users = response.data
-//                    this.setState(
-//                    {
-//                        'users': users,
-//                        'projects': projects,
-//                        'tasks': tasks
-//                    }
-//                )
+//    const requestUser = 'http://127.0.0.1:8000/api/users'
+//    const requestProject = 'http://127.0.0.1:8000/api/projects'
+//    const requestTask = 'http://127.0.0.1:8000/api/tasks'
+//
+//    axios.all(requestUser, requestTask, requestProject)
+//        .then(response => {
+//            axios.spread((...responses) => {
+//            const responseOne = responses[0];
+//            const responseTwo = responses[1];
+//            const responesThree = responses[2];
+//
+//            console.log(responseOne, responseTwo, responesThree);
+//            })
+//            )
 //            }).catch(error => console.log(error))
-//}
+//        }
 
 //    render () {
 //        return (
@@ -86,10 +116,6 @@ export default App;
 //        )
 //    }
 //
-
-
-
-
 
 
 
